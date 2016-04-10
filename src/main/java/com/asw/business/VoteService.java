574bf12cf7953608b0a9bbe0cc7bf286b22b4611
Lucas García <Lucas Garca@DESKTOP-TRU8GCQ>
2016-04-10 23:54:06 +0200

    Introducido metodo de fachada de obtencion y actualizacion de votos pendientes

--- src/main/java/com/asw/business/VoteService.java
+++ src/main/java/com/asw/business/VoteService.java
@@ -3,9 +3,10 @@ package com.asw.business;
 import java.util.List;
 
 import com.asw.model.Votacion;
+import com.asw.model.Voto;
 
 public interface VoteService {
 	
 	public List<Votacion> listAllVotaciones();
-
+	public List<Voto> getPendingVotes();
 }
--- src/main/java/com/asw/business/impl/VoteServiceImpl.java
+++ src/main/java/com/asw/business/impl/VoteServiceImpl.java
@@ -1,15 +1,33 @@
 package com.asw.business.impl;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.asw.business.VoteService;
 import com.asw.business.impl.votacion.ListVotacion;
 import com.asw.model.Votacion;
+import com.asw.model.Voto;
+import com.asw.persistence.impl.VotoGatewayImpl;
 
 public class VoteServiceImpl implements VoteService {
 
 	@Override
 	public List<Votacion> listAllVotaciones() {
 		return new ListVotacion().listAll();
+	}
+
+	@Override
+	public List<Voto> getPendingVotes() {
+		List<Voto> allPendingVotes = null;
+		VotoGatewayImpl votoUpd = new VotoGatewayImpl();
+		try {
+			allPendingVotes = new VotoGatewayImpl().findAll();
+			for(Voto voto:allPendingVotes){
+				votoUpd.actualizarLeido(voto.getId());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return allPendingVotes;
 	}
 }
