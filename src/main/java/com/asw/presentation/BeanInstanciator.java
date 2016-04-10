7388ddb77322912284ae663b587d4853987e53a0
Víctor Castaño Gutiérrez <victorcastanogutierrez@gmail.com>
2016-04-10 21:45:34 +0200

    DTO de voto

--- src/main/java/com/asw/presentation/BeanInstanciator.java
+++ src/main/java/com/asw/presentation/BeanInstanciator.java
@@ -37,7 +37,7 @@ public class BeanInstanciator {
 	@PostConstruct
 	public void init() {
 		System.out.println("BeanInstanciator - INIT");
-		//cargarTipoVotacion();
+		cargarTipoVotacion();
 	}
 
 	/**
