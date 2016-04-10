5254f2a7669ed4e43ea58786ca5ec0af84f8be58
Lucas García <Lucas Garca@DESKTOP-TRU8GCQ>
2016-04-10 23:57:49 +0200

    Fachada corregida

--- src/main/java/com/asw/business/impl/VoteServiceImpl.java
+++ src/main/java/com/asw/business/impl/VoteServiceImpl.java
@@ -1,13 +1,12 @@
 package com.asw.business.impl;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import com.asw.business.VoteService;
 import com.asw.business.impl.votacion.ListVotacion;
+import com.asw.business.impl.votacion.ListVotes;
 import com.asw.model.Votacion;
 import com.asw.model.Voto;
-import com.asw.persistence.impl.VotoGatewayImpl;
 
 public class VoteServiceImpl implements VoteService {
 
@@ -18,16 +17,6 @@ public class VoteServiceImpl implements VoteService {
 
 	@Override
 	public List<Voto> getPendingVotes() {
-		List<Voto> allPendingVotes = null;
-		VotoGatewayImpl votoUpd = new VotoGatewayImpl();
-		try {
-			allPendingVotes = new VotoGatewayImpl().findAll();
-			for(Voto voto:allPendingVotes){
-				votoUpd.actualizarLeido(voto.getId());
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return allPendingVotes;
+		return new ListVotes().listPendingVotes();
 	}
 }
---
+++ src/main/java/com/asw/business/impl/votacion/ListVotes.java
@@ -0,0 +1,24 @@
+package com.asw.business.impl.votacion;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.asw.model.Voto;
+import com.asw.persistence.impl.VotoGatewayImpl;
+
+public class ListVotes {
+
+	public List<Voto> listPendingVotes() {
+		List<Voto> allPendingVotes = null;
+		VotoGatewayImpl votoUpd = new VotoGatewayImpl();
+		try {
+			allPendingVotes = new VotoGatewayImpl().findAll();
+			for(Voto voto:allPendingVotes){
+				votoUpd.actualizarLeido(voto.getId());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return allPendingVotes;
+	}
+}
